bb910bfcfc5d92ea80687aca7e8726fc9b72d3a3
injamamulhak0804 <injamamulhak0804@gmail.com>
2024-08-08 10:06:44 +0530

    learn Py

---
+++ string.py
@@ -0,0 +1,68 @@
+data = ("John", "Doe", 53.44)
+format_string = "Hello"
+#print(format_string % data)
+
+
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your current balance is $%s."
+
+#print(format_string % data)
+
+
+#Slicing Operator [start, stop, step]
+
+str = "zamamul_hak"
+"""
+print(str[3])
+print(str[3:9])
+print(str[3:9:2])
+print(str[3:])
+print(str[::-1])
+"""
+
+#            TEST
+
+s = "Hey thera! what shou"
+# Length should be 20
+print("Length of s = %d" % len(s))
+print(s.index('a'))
+print("a occurs %d times" % s.count("a"))
+print("String in uppercase: %s" % s.upper())
+
+
+
+
+s = "Hey thera! what shou"
+# Length should be 20
+print("Length of s = %s" % len(s))
+
+# First occurrence of "a" should be at index 8
+print("The first occurrence of the letter a = %d" % s.index("a"))
+
+# Number of a's should be 2
+print("a occurs %d times" % s.count("a"))
+
+# Slicing the string into bits
+print("The first five characters are '%s'" % s[:5]) # Start to 5
+print("The next five characters are '%s'" % s[5:11]) # 5 to 10
+print("The thirteenth character is '%s'" % s[13]) # Just number 12
+print("The characters with odd index are '%s'" %s[0::2]) #(0-based indexing)
+print("The last five characters are '%s'" % s[:-5]) # 5th-from-last to end
+
+# Convert everything to uppercase
+print("String in uppercase: %s" % s.upper())
+
+# Convert everything to lowercase
+print("String in lowercase: %s" % s.lower())
+
+# Check how a string starts
+if s.startswith("Hey"):
+    print("String starts with 'Str'. Good!")
+
+# Check how a string ends
+if s.endswith("shou"):
+    print("String ends with 'ome!'. Good!")
+
+# Split the string into three separate strings,
+# each containing only a word
+print("Split the words of the string: %s" % s.split(" "))
